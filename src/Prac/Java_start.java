6241867d1a28f5cf376fca63ea2b015853e6b6e7
SayantanAdhikary <sayantanad@gmail.com>
2021-09-07 17:59:37 +0530

    new changes

--- src/Prac/Java_start.java
+++ src/Prac/Java_start.java
@@ -2,8 +2,12 @@ package Prac;
 
 public class Java_start {
 
+	//private static final String //ServicePointManager = null;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//ServicePointManager.Expect100Continue = true;
+		//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 		System.out.println("Hello");
 
 	}
